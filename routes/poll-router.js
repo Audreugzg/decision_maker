6955ebdcbf988cfa2c8d071ac162c8ff511ffd89
Payam Beigi <beigi.payam@gmail.com>
2022-07-04 10:16:31 +0000

    Add Poll routes

---
+++ routes/poll-router.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const router  = express.Router();
+const pollQueries = require('../db/poll-queries'); // ADD DB QUERIES
+
+module.exports = (db) => {
+
+  router.get("/create", (req, res) => {
+    res.render("form");
+  });
+  router.post("/create", (req, res) => {
+    pollQueries.createPoll()
+  });
+  router.get("/:id", (req, res) => {
+    pollQueries.getPollResult(req.params.id)
+      .then((result) => {
+        res.json(result);
+      })
+  });
+
+  return router;
+};
